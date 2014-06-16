fbdca5c138463325728b9fd050e052c0526ece31
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-16 14:50:59 -0700

    Transcompilation: Convert from one source code to another

---
+++ chap01/Transcompile CoffeeScript/Gruntfile.js
@@ -0,0 +1,28 @@
+//Code example 05-coffeescript
+module.exports = function(grunt) {
+
+  // Load the plugin that provides the "coffee" task.
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+
+  // Project configuration.
+  grunt.initConfig({
+    coffee: {
+      target1: {
+        expand: true,
+        flatten: true,
+        cwd: 'src/',
+        src: ['*.coffee'],
+        dest: 'build/',
+        ext: '.js'
+      },
+      target2: {
+        files: {
+          'build/bazz.js': 'src/*.coffee'
+        }
+      }
+    }
+  });
+
+  // Define the default task
+  grunt.registerTask('default', ['coffee']);
+};
