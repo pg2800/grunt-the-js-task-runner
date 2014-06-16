e4564fde0d3b0837eddb50773b8d162bb5c8bba3
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-16 16:00:10 -0700

    Joining Files together

---
+++ chap01/Concatenate/Gruntfile.js
@@ -0,0 +1,20 @@
+//Code example 08-concatenate
+module.exports = function(grunt) {
+
+  // Load the plugin that provides the "concat" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Project configuration.
+  grunt.initConfig({
+    concat: {
+      target1: {
+        files: {
+          "build/abc.js": ["src/a.js", "src/b.js", "src/c.js"]
+        }
+      }
+    }
+  });
+
+  // Define the default task
+  grunt.registerTask('default', ['concat']);
+};
