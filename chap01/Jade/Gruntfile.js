8f579e69aa1c2ee41c3fa6ba056aa89340a96233
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-16 15:03:06 -0700

    Compiles to HTML

---
+++ chap01/Jade/Gruntfile.js
@@ -0,0 +1,21 @@
+//Code example 06-jade
+module.exports = function(grunt) {
+
+  // Load the plugin that provides the "jade" task.
+  grunt.loadNpmTasks('grunt-contrib-jade');
+
+  // Project configuration.
+  grunt.initConfig({
+    jade: {
+      target1: {
+        files: {
+          "build/foo.html": "src/foo.jade",
+          "build/bar.html": "src/bar.jade"
+        } 
+      }
+    }
+  });
+
+  // Define the default task
+  grunt.registerTask('default', ['jade']);
+};
