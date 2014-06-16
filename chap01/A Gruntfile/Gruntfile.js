41cff64951f1dfa57a304b6a0c773d3fbe27b090
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-16 11:46:23 -0700

    Changing the name of exercises

---
+++ chap01/A Gruntfile/Gruntfile.js
@@ -0,0 +1,19 @@
+//Code example 01-minify
+module.exports = function(grunt) {
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Project configuration.
+  grunt.initConfig({
+    uglify: {
+      target1: {
+        src: 'foo.js',
+        dest: 'foo.min.js'
+      }
+    }
+  });
+
+  // Define the default task
+  grunt.registerTask('default', ['uglify']);
+};
