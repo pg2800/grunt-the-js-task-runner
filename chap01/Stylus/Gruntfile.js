9aa554089b0daada04715f4d9041dd59cef22f9c
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-16 15:33:18 -0700

    Transcopiled to CSS

---
+++ chap01/Stylus/Gruntfile.js
@@ -0,0 +1,20 @@
+//Code example 07-stylus
+module.exports = function(grunt) {
+
+  // Load the plugin that provides the "stylus" task.
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  // Project configuration.
+  grunt.initConfig({
+    stylus: {
+      target1: {
+        files: {
+          "build/foo.css": "src/foo.styl"
+        }
+      }
+    }
+  });
+
+  // Define the default task
+  grunt.registerTask('default', ['stylus']);
+};
