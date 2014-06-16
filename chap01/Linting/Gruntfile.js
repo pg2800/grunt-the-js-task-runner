2eb13c0f97e38bef01cab12c6e69bd15c8208f72
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-16 14:25:30 -0700

    Process of finding probable bugs.. and/or errors

---
+++ chap01/Linting/Gruntfile.js
@@ -0,0 +1,25 @@
+//Code example 04-linting
+//Gruntfile.js
+module.exports = function(grunt) {
+
+  // Load the plugin that provides the "jshint" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true
+      },
+      target1: ['Gruntfile.js', 'src/**/*.js']
+    }
+  });
+
+  // Define the default task
+  grunt.registerTask('default', ['jshint']);
+
+};
+
+//src/foo.js
+if(7 == "7") alert(42); 
