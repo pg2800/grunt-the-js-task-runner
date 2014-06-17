3490f51b28c7d528107347611254b52316331743
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-17 11:19:24 -0700

    Adding a test of my own to practice

--- chap01/test/Gruntfile.js
+++ chap01/test/Gruntfile.js
@@ -1,3 +1,32 @@
 module.exports = function (grunt){
-	console.log(grunt);
+	grunt
+	.initConfig({
+		pkg: grunt.file.readJSON('package.json')
+		,uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %>'
+				+ ' <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			}
+			,dist: {
+				src: '<%= pkg.name %>.js'
+				,dest: '<%= pkg.name %>.<%= pkg.version %>.min.js'
+			}
+		}
+		,cssmin: {
+			compress: {
+				options: {
+					banner: '<%= banner %>'
+				}
+				,files: {
+					'project.min.css': ['1.css', '2.css'/*, '...'*/]
+				}
+			}
+		}
+	})
+	.loadNpmTasks('')
+	.registerTask("default");
+	// grunt.loadNpmTasks('grunt-contrib-uglify');
+	// grunt.loadNpmTasks('grunt-contrib-cssmin');
+	// grunt.loadNpmTasks('grunt-contrib-jshint');
+	// grunt.registerTask("default", ['jshint', 'uglify']);
 };
