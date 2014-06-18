b97abe0ad5acbfa94e56ce5f7386033aa276a927
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-18 09:29:44 -0700

    This is a non running test, to see how to install grunt and its dependencies

--- chap01/test/Gruntfile.js
+++ chap01/test/Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function (grunt){
-	grunt
-	.initConfig({
+	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json')
 		,uglify: {
 			options: {
@@ -18,15 +17,33 @@ module.exports = function (grunt){
 					banner: '<%= banner %>'
 				}
 				,files: {
+					// This is creating a project.min.css from 1, 2 IN ORDER
 					'project.min.css': ['1.css', '2.css'/*, '...'*/]
 				}
 			}
 		}
-	})
-	.loadNpmTasks('')
-	.registerTask("default");
-	// grunt.loadNpmTasks('grunt-contrib-uglify');
-	// grunt.loadNpmTasks('grunt-contrib-cssmin');
-	// grunt.loadNpmTasks('grunt-contrib-jshint');
-	// grunt.registerTask("default", ['jshint', 'uglify']);
+		,imagemin: {
+			dist: {
+				options: {
+					optimizationLevel: 3
+				}
+				,files: {
+					// 'destination': 'source'
+					'dist/img.png': 'src'
+					,'dist/img.jpg': 'src/img.jpg'
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	// Comments out the code and also the readme for Github
+	grunt.loadNpmTasks('grunt-markdown'); 
+	// Sometimes when we debug we forget about console.logs
+	// This removes the logs and alerts of anything that might look like a log
+	// It is clever
+	grunt.loadNpmTasks('grunt-remove-logging'); 
+	grunt.registerTask("default", ['jshint', 'uglify', 'cssmin', 'watch', 'markdown', 'removelogging']);
 };
