cbd863ee5a884fbf967107d3609a564395bbc345
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-06-25 08:48:23 -0700

    Modified tests, increased complexity

--- chap04/quickSort/spec/quickSort.spec.js
+++ chap04/quickSort/spec/quickSort.spec.js
@@ -1,12 +1,28 @@
 describe("Quick Sort Algorithm", function() {
-	var arr, result;
+	var simpleUnordered, repeatedValues, negativeValues;
 	beforeEach(function() {
-		arr = [1,7,6,2,4,9,5,3,8];
-		result = [1,2,3,4,5,6,7,8,9];
+		simpleUnordered = {
+			arr: [1,7,6,2,4,9,5,0,3,8]
+			,result: [0,1,2,3,4,5,6,7,8,9]
+		}
+		repeatedValues = {
+			arr: [3,1,7,6,2,8,4,0,9,7,5,3,8]
+			,result: [0,1,2,3,3,4,5,6,7,7,8,8,9]
+		}
+		negativeValues = {
+			arr: [-3,1,7,-6,0,2,8,4,9,7,5,3,8]
+			,result: [-6,-3,0,1,2,3,4,5,7,7,8,8,9]
+		}
 	});
 	describe("Actual Result", function() {
 		it("should sort an unordered array", function() {
-			expect(quickSort(arr)).toEqual(result);
+			expect(quickSort(simpleUnordered.arr)).toEqual(simpleUnordered.result);
+		});
+		it("should sort repeated values", function() {
+			expect(quickSort(repeatedValues.arr)).toEqual(repeatedValues.result);
+		});
+		it("should sort negative values", function() {
+			expect(quickSort(negativeValues.arr)).toEqual(negativeValues.result);
 		});
 	});
 	
